5660fdadfbc5886291ad79b3dbaf3f56be48631c
mysticthrive <thrivemystic@gmail.com>
2025-06-02 16:35:57 +0330

    create fake data

--- app-backend/config/settings.py
+++ app-backend/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'rest_framework_simplejwt',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -53,7 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
--- app-backend/menu/admin.py
+++ app-backend/menu/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import MenuItem, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title",)
+    ordering = ("title",)
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "stock", "status", "is_featured", "created_date")
+    list_filter = ("status", "is_featured", "category")
+    search_fields = ("title", "description")
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ("category",)
+    readonly_fields = ("views", "created_date", "updated_date")
+    fieldsets = (
+        ("اطلاعات کلی", {
+            "fields": ("title", "slug", "description", "category", "user")
+        }),
+        ("تصاویر", {
+            "fields": ("image1", "image2")
+        }),
+        ("وضعیت و قیمت", {
+            "fields": ("price", "discount_percent", "stock", "status", "is_featured")
+        }),
+        ("سایر", {
+            "fields": ("views", "preparation_time", "created_date", "updated_date")
+        }),
+    )
