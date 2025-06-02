9fa7128d90402b49377ca0ec4e23ea25bc54b66f
mysticthrive <thrivemystic@gmail.com>
2025-06-02 17:39:41 +0330

    axios

--- app-frontend/src/components/Menu.js
+++ app-frontend/src/components/Menu.js
@@ -1,369 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Menu() {
-  return (
-   
-        
-    <section id="menu" className="menu section">
+  const [menuItems, setMenuItems] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("starters"); // دسته پیش‌فرض
 
-     
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/v1/menu-items/") // آدرس API خودت رو بذار
+      .then((response) => {
+        setMenuItems(response.data);
+      })
+      .catch((error) => {
+        console.error("❌ خطا در دریافت منو:", error);
+      });
+  }, []);
+
+  // گروه‌بندی آیتم‌ها بر اساس دسته‌بندی
+  const groupedItems = menuItems.reduce((acc, item) => {
+    item.category.forEach((cat) => {
+      if (!acc[cat.slug]) acc[cat.slug] = [];
+      acc[cat.slug].push(item);
+    });
+    return acc;
+  }, {});
+
+  const categories = Object.keys(groupedItems);
+
+  return (
+    <section id="menu" className="menu section">
       <div className="container section-title" data-aos="fade-up">
         <h2>Our Menu</h2>
         <p><span>Check Our</span> <span className="description-title">Yummy Menu</span></p>
       </div>
+
       <div className="container">
-
         <ul className="nav nav-tabs d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-
-          <li className="nav-item">
-            <a className="nav-link active show" data-bs-toggle="tab" data-bs-target="#menu-starters">
-              <h4>Starters</h4>
-            </a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" data-bs-toggle="tab" data-bs-target="#menu-breakfast">
-              <h4>Breakfast</h4>
-            </a>
-
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" data-bs-toggle="tab" data-bs-target="#menu-lunch">
-              <h4>Lunch</h4>
-            </a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" data-bs-toggle="tab" data-bs-target="#menu-dinner">
-              <h4>Dinner</h4>
-            </a>
-          </li>
-
+          {categories.map((slug) => (
+            <li className="nav-item" key={slug}>
+              <button
+                className={`nav-link ${activeCategory === slug ? "active show" : ""}`}
+                onClick={() => setActiveCategory(slug)}
+              >
+                <h4>{groupedItems[slug][0]?.category.find((c) => c.slug === slug)?.title || slug}</h4>
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="tab-content" data-aos="fade-up" data-aos-delay="200">
-
-          <div className="tab-pane fade active show" id="menu-starters">
-
+          <div className="tab-pane fade active show">
             <div className="tab-header text-center">
               <p>Menu</p>
-              <h3>Starters</h3>
+              <h3>{activeCategory}</h3>
             </div>
 
             <div className="row gy-5">
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-1.png" className="glightbox"><img src="assets/img/menu/menu-item-1.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Magnam Tiste</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $5.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-2.png" className="glightbox"><img src="assets/img/menu/menu-item-2.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Aut Luia</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $14.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-3.png" className="glightbox"><img src="assets/img/menu/menu-item-3.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Est Eligendi</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $8.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-4.png" className="glightbox"><img src="assets/img/menu/menu-item-4.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-5.png" className="glightbox"><img src="assets/img/menu/menu-item-5.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-6.png" className="glightbox"><img src="assets/img/menu/menu-item-6.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Laboriosam Direva</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $9.95
-                </p>
-              </div>
-
+              {groupedItems[activeCategory]?.map((item) => (
+                <div className="col-lg-4 menu-item" key={item.id}>
+                  <a href={item.image1} className="glightbox">
+                    <img src={item.image1} className="menu-img img-fluid" alt={item.title} />
+                  </a>
+                  <h4>{item.title}</h4>
+                  <p className="ingredients">{item.description}</p>
+                  <p className="price">{item.get_price} تومان</p>
+                </div>
+              ))}
             </div>
           </div>
-
-          <div className="tab-pane fade" id="menu-breakfast">
-
-            <div className="tab-header text-center">
-              <p>Menu</p>
-              <h3>Breakfast</h3>
-            </div>
-
-            <div className="row gy-5">
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-1.png" className="glightbox"><img src="assets/img/menu/menu-item-1.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Magnam Tiste</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $5.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-2.png" className="glightbox"><img src="assets/img/menu/menu-item-2.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Aut Luia</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $14.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-3.png" className="glightbox"><img src="assets/img/menu/menu-item-3.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Est Eligendi</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $8.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-4.png" className="glightbox"><img src="assets/img/menu/menu-item-4.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-5.png" className="glightbox"><img src="assets/img/menu/menu-item-5.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-6.png" className="glightbox"><img src="assets/img/menu/menu-item-6.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Laboriosam Direva</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $9.95
-                </p>
-              </div>
-
-            </div>
-          </div>
-
-          <div className="tab-pane fade" id="menu-lunch">
-
-            <div className="tab-header text-center">
-              <p>Menu</p>
-              <h3>Lunch</h3>
-            </div>
-
-            <div className="row gy-5">
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-1.png" className="glightbox"><img src="assets/img/menu/menu-item-1.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Magnam Tiste</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $5.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-2.png" className="glightbox"><img src="assets/img/menu/menu-item-2.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Aut Luia</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $14.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-3.png" className="glightbox"><img src="assets/img/menu/menu-item-3.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Est Eligendi</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $8.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-4.png" className="glightbox"><img src="assets/img/menu/menu-item-4.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-5.png" className="glightbox"><img src="assets/img/menu/menu-item-5.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-6.png" className="glightbox"><img src="assets/img/menu/menu-item-6.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Laboriosam Direva</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $9.95
-                </p>
-              </div>
-
-            </div>
-          </div>
-
-          <div className="tab-pane fade" id="menu-dinner">
-
-            <div className="tab-header text-center">
-              <p>Menu</p>
-              <h3>Dinner</h3>
-            </div>
-
-            <div className="row gy-5">
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-1.png" className="glightbox"><img src="assets/img/menu/menu-item-1.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Magnam Tiste</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $5.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-2.png" className="glightbox"><img src="assets/img/menu/menu-item-2.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Aut Luia</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $14.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-3.png" className="glightbox"><img src="assets/img/menu/menu-item-3.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Est Eligendi</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $8.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-4.png" className="glightbox"><img src="assets/img/menu/menu-item-4.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-5.png" className="glightbox"><img src="assets/img/menu/menu-item-5.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Eos Luibusdam</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $12.95
-                </p>
-              </div>
-
-              <div className="col-lg-4 menu-item">
-                <a href="assets/img/menu/menu-item-6.png" className="glightbox"><img src="assets/img/menu/menu-item-6.png" className="menu-img img-fluid" alt=""/></a>
-                <h4>Laboriosam Direva</h4>
-                <p className="ingredients">
-                  Lorem, deren, trataro, filede, nerada
-                </p>
-                <p className="price">
-                  $9.95
-                </p>
-              </div>
-            </div>
-          </div>
-
         </div>
-
       </div>
-
     </section>
-
-   
-
-   
-
   );
 }
 
